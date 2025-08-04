21375a16bd70688a0da6c9ce60803e9ce3f544f8
Charlene Costa <charlenecosta81@gamil.com>
2025-08-04 16:58:09 -0300

    implementar codigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,34 +1,3 @@
-/*let nomes = [];
-
-function adicionarNome(){
-    const input = document.getElementById("nomeInput");
-    const nome = input. value .trim();
-    
-    if(nome === ""){
-        alert("Por favor, digite um nome válido.");
-        return;
-    }
-    nomes.push(nome);
-    atualizarLista();
-    input.value = "";
-}
-function atualizarLista(){
-    const lista = document.getElementById("listaNome");
-    lista.innerHTML = "";
-    nomes.forEach((nome)=>{
-        const item = document.createElement("li");
-        item.textContent = nome;
-        lista.appendChild(item);
-    });
-}
-function sortearAmigo(){
-    if(nomes.length === 0){
-        alert ("Lista está vazia.Adicione nomes antes de sortear.");
-        return;
-    }
-    const sorteado = nomes[Math.floor(Math.random()*nomes.length)];
-    document.getElementById("resultado").textContent = `O amigo secreto é: ${sorteado}`;
-}*/
 const listaDeAmigos = [];
 
 function adicionarAmigo() {
@@ -37,6 +6,10 @@ function adicionarAmigo() {
 
     if (nome === "") {
         alert("Por favor, digite um nome válido.");
+        return;
+    }
+    if (listaDeAmigos.includes(nome)) {
+        alert("Este nome já foi adicionado à lista.");
         return;
     }
 
